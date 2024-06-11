5929a20de7e5a722052d58f080fbf9b21df1eaf2
Sumanyu301 <sumanyukodan301@gmail.com>
2024-06-11 15:30:54 +0530

    Added missing header files and define constants for cache size and maximum clients

--- prxy_server_with_cache.c
+++ prxy_server_with_cache.c
@@ -1,7 +1,22 @@
-#include<stdio.h>
-#include<string.h>
 #include "proxy_parse.h"
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+#define MAX_CLIENTS 1000
+#define MAX_CACHE_SIZE 1000000
 
 struct cacheElement
 {
@@ -12,8 +27,12 @@ struct cacheElement
     struct cacheElement *next;
 };
 
-struct cacheElement* find(char *url);
-int add_cacheElement(char *data, int size,char *url);
+struct cacheElement *find(char *url);
+int add_cacheElement(char *data, int size, char *url);
 void remove_cacheElement();
 
-int port_number=8080;
+int port_number = 8080;
+int proxy_socketId;
+pthread_t thread_id[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
